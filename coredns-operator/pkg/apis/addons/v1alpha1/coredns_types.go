e720c869684606eb79afaac74e5037c4dc6e40ee
leigh capili <leigh@null.net>
2019-05-01 12:09:19 -0600

    operator-sdk add api --api-version=addons.k8s.io/v1alpha1 --kind=CoreDNS

---
+++ coredns-operator/pkg/apis/addons/v1alpha1/coredns_types.go
@@ -0,0 +1,49 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CoreDNSSpec defines the desired state of CoreDNS
+// +k8s:openapi-gen=true
+type CoreDNSSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// CoreDNSStatus defines the observed state of CoreDNS
+// +k8s:openapi-gen=true
+type CoreDNSStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CoreDNS is the Schema for the coredns API
+// +k8s:openapi-gen=true
+type CoreDNS struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CoreDNSSpec   `json:"spec,omitempty"`
+	Status CoreDNSStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CoreDNSList contains a list of CoreDNS
+type CoreDNSList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CoreDNS `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CoreDNS{}, &CoreDNSList{})
+}
