2385c56a496774c9a0837a0cf5bcec304a34bf67
Jeff Johnson <jrjohnson@google.com>
2019-10-14 20:40:57 +0000

    kubeproxy: regenerate boilerplate w/ plugin fix
    regenerate controller/api code with patch[1]
    
    [1] https://github.com/kubernetes-sigs/kubebuilder/pull/1059

--- kubeproxy/controllers/kubeproxy_controller.go
+++ kubeproxy/controllers/kubeproxy_controller.go
@@ -1,50 +1,71 @@
-/*
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package controllers
 
 import (
-	"context"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon/pkg/status"
+	"sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/declarative"
 
 	"github.com/go-logr/logr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	addonsv1alpha1 "addon-operators/kubeproxy/api/v1alpha1"
+	api "addon-operators/kubeproxy/api/v1alpha1"
 )
+
+var _ reconcile.Reconciler = &KubeProxyReconciler{}
 
 // KubeProxyReconciler reconciles a KubeProxy object
 type KubeProxyReconciler struct {
 	client.Client
 	Log logr.Logger
+
+	declarative.Reconciler
 }
 
 // +kubebuilder:rbac:groups=addons.x-k8s.io,resources=kubeproxies,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=addons.x-k8s.io,resources=kubeproxies/status,verbs=get;update;patch
 
-func (r *KubeProxyReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("kubeproxy", req.NamespacedName)
-
-	// your logic here
-
-	return ctrl.Result{}, nil
-}
-
 func (r *KubeProxyReconciler) SetupWithManager(mgr ctrl.Manager) error {
-	return ctrl.NewControllerManagedBy(mgr).
-		For(&addonsv1alpha1.KubeProxy{}).
-		Complete(r)
+	addon.Init()
+
+	labels := map[string]string{
+		"k8s-app": "kubeproxy",
+	}
+
+	watchLabels := declarative.SourceLabel(mgr.GetScheme())
+
+	if err := r.Reconciler.Init(mgr, &api.KubeProxy{},
+		declarative.WithObjectTransform(declarative.AddLabels(labels)),
+		declarative.WithOwner(declarative.SourceAsOwner),
+		declarative.WithLabels(watchLabels),
+		declarative.WithStatus(status.NewBasic(mgr.GetClient())),
+		// TODO: add an application to your manifest:  declarative.WithObjectTransform(addon.TransformApplicationFromStatus),
+		// TODO: add an application to your manifest:  declarative.WithManagedApplication(watchLabels),
+		declarative.WithObjectTransform(addon.ApplyPatches),
+	); err != nil {
+		return err
+	}
+
+	c, err := controller.New("kubeproxy-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to KubeProxy
+	err = c.Watch(&source.Kind{Type: &api.KubeProxy{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to deployed objects
+	_, err = declarative.WatchAll(mgr.GetConfig(), c, r, watchLabels)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- kubeproxy/go.mod
+++ kubeproxy/go.mod
@@ -2,4 +2,12 @@ module addon-operators/kubeproxy
 
 go 1.12
 
-require sigs.k8s.io/controller-runtime v0.2.2
+require (
+	github.com/go-logr/logr v0.1.0
+	github.com/onsi/ginkgo v1.7.0
+	github.com/onsi/gomega v1.4.3
+	k8s.io/apimachinery v0.0.0-20190404173353-6a84e37a896d
+	k8s.io/client-go v11.0.1-0.20190409021438-1a26190bd76a+incompatible
+	sigs.k8s.io/controller-runtime v0.2.2
+	sigs.k8s.io/kubebuilder-declarative-pattern v0.0.0-20190926123507-e845b6c6f25a
+)
