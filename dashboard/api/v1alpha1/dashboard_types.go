3c8ecf1404096b8cacc75af98b29851fb383bf89
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-02-28 02:33:45 +0100

    Initial Dashboard scaffolding

---
+++ dashboard/api/v1alpha1/dashboard_types.go
@@ -0,0 +1,72 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	addonv1alpha1 "sigs.k8s.io/kubebuilder-declarative-pattern/pkg/patterns/addon/pkg/apis/v1alpha1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// DashboardSpec defines the desired state of Dashboard
+type DashboardSpec struct {
+	addonv1alpha1.CommonSpec `json:",inline"`
+	addonv1alpha1.PatchSpec  `json:",inline"`
+
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// DashboardStatus defines the observed state of Dashboard
+type DashboardStatus struct {
+	addonv1alpha1.CommonStatus `json:",inline"`
+
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+
+// Dashboard is the Schema for the dashboards API
+type Dashboard struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   DashboardSpec   `json:"spec,omitempty"`
+	Status DashboardStatus `json:"status,omitempty"`
+}
+
+var _ addonv1alpha1.CommonObject = &Dashboard{}
+
+func (o *Dashboard) ComponentName() string {
+	return "dashboard"
+}
+
+func (o *Dashboard) CommonSpec() addonv1alpha1.CommonSpec {
+	return o.Spec.CommonSpec
+}
+
+func (o *Dashboard) PatchSpec() addonv1alpha1.PatchSpec {
+	return o.Spec.PatchSpec
+}
+
+func (o *Dashboard) GetCommonStatus() addonv1alpha1.CommonStatus {
+	return o.Status.CommonStatus
+}
+
+func (o *Dashboard) SetCommonStatus(s addonv1alpha1.CommonStatus) {
+	o.Status.CommonStatus = s
+}
+
+// +kubebuilder:object:root=true
+
+// DashboardList contains a list of Dashboard
+type DashboardList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Dashboard `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Dashboard{}, &DashboardList{})
+}
---
+++ dashboard/go.mod
@@ -0,0 +1,13 @@
+module sigs.k8s.io/cluster-addons/dashboard
+
+go 1.13
+
+require (
+	github.com/go-logr/logr v0.1.0
+	github.com/onsi/ginkgo v1.10.1
+	github.com/onsi/gomega v1.7.0
+	k8s.io/apimachinery v0.0.0-20190913080033-27d36303b655
+	k8s.io/client-go v0.0.0-20190918160344-1fbdaa4c8d90
+	sigs.k8s.io/controller-runtime v0.4.0
+	sigs.k8s.io/kubebuilder-declarative-pattern v0.0.0-20200226054827-748a6481b2a4
+)
