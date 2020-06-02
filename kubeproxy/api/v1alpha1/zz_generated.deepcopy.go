2eddc479e2fb78e65a9afe3070fe1117b15f2571
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-06-02 02:41:42 +0100

    Makes controller run in-cluster

--- kubeproxy/api/v1alpha1/zz_generated.deepcopy.go
+++ kubeproxy/api/v1alpha1/zz_generated.deepcopy.go
@@ -54,7 +54,7 @@ func (in *KubeProxy) DeepCopyObject() runtime.Object {
 func (in *KubeProxyList) DeepCopyInto(out *KubeProxyList) {
 	*out = *in
 	out.TypeMeta = in.TypeMeta
-	out.ListMeta = in.ListMeta
+	in.ListMeta.DeepCopyInto(&out.ListMeta)
 	if in.Items != nil {
 		in, out := &in.Items, &out.Items
 		*out = make([]KubeProxy, len(*in))
