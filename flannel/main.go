6f7e562b2c0bed154b40917aa149488c950aff4c
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-06-17 01:46:56 +0100

    helps operator run in-cluster

--- flannel/main.go
+++ flannel/main.go
@@ -57,6 +57,7 @@ func main() {
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		LeaderElection:     enableLeaderElection,
+		LeaderElectionID:   "flannel-operator",
 		Port:               9443,
 	})
 	if err != nil {
