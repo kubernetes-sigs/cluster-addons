41861f0b54bd977a9645057d316b0fe087280a49
Somtochi Onyekwere <somtochionyekwere@gmail.com>
2020-07-07 08:11:25 +0100

    Example kubectl plugin

--- tools/kubectl-ownerref/go.mod
+++ tools/kubectl-ownerref/go.mod
@@ -1,3 +1,14 @@
 module sigs.k8s.io/cluster-addons/tools/kubectl-ownerref
 
 go 1.13
+
+require (
+	github.com/imdario/mergo v0.3.9 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20200625001655-4c5254603344 // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/apiextensions-apiserver v0.18.5
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/utils v0.0.0-20200619165400-6e3d28b6ed19 // indirect
+)
--- tools/kubectl-ownerref/main.go
+++ tools/kubectl-ownerref/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 	"os"
 )
 
 func main() {
+	kubeconfig := flag.String("kubeconfig", "~/.kube/config", "kubeconfig file")
+
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	clientset, err := kubernetes.NewForConfig(config)
+
+	pod, err := clientset.CoreV1().Pods("").Get("kindnet-j4dpv", metav1.GetOptions{})
 	args := os.Args[1:]
 
 	if args[0] == "config" {
